0e21e26ee53d2bacae1596595ac52085df3ea5e4
A.L. Blackwell <ahvblackwelltech@gmail.com>
2018-11-12 13:29:24 -0600

    Fixed Bugs

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 keys = {
-    "product_url": "https://www.supremenewyork.com/shop/accessories/ls6jtnd1c",
+    "product_url": "https://www.supremenewyork.com/shop/accessories/n5ky7ecs8",
     "name": "Ahvi Blackwell",
     "email": "alblackwelldev@outlook.com",
     "phone_number": 4152637658,
     "address": "1234 Grewnway Dr",
+    "city": "El Sobrante",
+    "state": "CA",
     "zip": "94803",
     "card_number": 423264578978
 }
