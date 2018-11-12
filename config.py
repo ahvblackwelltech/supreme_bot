1d4d67ec35985cff1a481332574c22dc73b66576
A.L. Blackwell <ahvblackwelltech@gmail.com>
2018-11-12 13:18:19 -0600

    Fixed Bug

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ keys = {
     "email": "alblackwelldev@outlook.com",
     "phone_number": 4152637658,
     "address": "1234 Grewnway Dr",
-    "zip": "94803"
-    "card_number": 
+    "zip": "94803",
+    "card_number": 423264578978
 }
