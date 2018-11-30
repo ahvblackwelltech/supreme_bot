975c3315c54a37ea788998fb1a2050c02b12b8b9
A.L. Blackwell <ahvblackwelltech@gmail.com>
2018-11-30 08:17:24 -0600

    Fixed Bug

--- bot.py
+++ bot.py
@@ -11,15 +11,31 @@ def order(k):
 	driver.find_element_by_xpath('//*[@id="add-remove-buttons"]/input').click()
 	time.sleep(1)
 	driver.find_element_by_xpath('//*[@id="cart"]/a[2]').click()
+	# name
 	driver.find_element_by_xpath('//*[@id="order_billing_name"]').send_keys(k["name"])
+	# email
 	driver.find_element_by_xpath('//*[@id="order_email"]').send_keys(k["email"])
+	# phone
 	driver.find_element_by_xpath('//*[@id="order_tel"]').send_keys(k["phone_number"])
+	# address
 	driver.find_element_by_xpath('//*[@id="bo"]').send_keys(k["address"])
+	# zip
 	driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(k["zip"])
+	# city
 	driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(k["city"])
+	# state
 	driver.find_element_by_xpath('//*[@id="order_billing_state"]').send_keys(k["state"])
+	# card information
 	driver.find_element_by_xpath('//*[@id="nnaerb"]').send_keys(k["card_number"])
-
+	driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(k["card_cvv"])
+	# card month
+	driver.find_element_by_xpath('//*[@id="credit_card_month"]/option[4]').format(k["credit_card_month"]).click()
+	#card year 
+	driver.find_element_by_xpath('//*[@id="credit_card_year"]/option[6]').format(k["credit_card_year"]).click()
+	# terms and conditions
+	driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins').click()
+	# process payment
+	driver.find_element_by_xpath('//*[@id="pay"]/input').click()
 
 if __name__ == '__main__':
 	order(keys)
--- config.py
+++ config.py
@@ -1,11 +1,19 @@
+"""
+Currently as of 11/30/2018, supreme allows for 2018-2028 as card expiration years 
+There year are 1-10 in the xpath selector for that dropdown.
+"""
+
 keys = {
-    "product_url": "https://www.supremenewyork.com/shop/accessories/n5ky7ecs8",
+    "product_url": "https://www.supremenewyork.com/shop/accessories/oypgt0xhn",
     "name": "Ahvi Blackwell",
     "email": "alblackwelldev@outlook.com",
     "phone_number": 4152637658,
     "address": "1234 Grewnway Dr",
-    "city": "El Sobrante",
+    "city": "",
     "state": "CA",
-    "zip": "94803",
-    "card_number": 423264578978
+    "zip": 94803,
+    "card_number": 4232783632431124,
+    "card_cvv": 323,
+    "credit_card_month": 4,
+    "credit_card_year": 6 # see docs above
 }
